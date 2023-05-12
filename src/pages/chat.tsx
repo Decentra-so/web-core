a1182ff381ff68b421c2b8306d4fd1e3bbd2a1e7
Daniel <95552251+d3centra@users.noreply.github.com>
2023-05-12 23:03:19 +0200

    Update chat.tsx

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -460,7 +460,7 @@ const Chat = () => {
                 boxSizing: 'border-box',
                 height: 'calc(100vh - var(--header-height) - 24px)',
                 top: 'var(--header-height)',
-                margin: '12px 0'
+                margin: '12px 0',
                 boxShadow: '0 0 5px #56515191',
               },
             }}
