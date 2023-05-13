9cc61e3f92c8119732aba2ee6ab489da01a01335
Daniel <95552251+d3centra@users.noreply.github.com>
2023-05-13 13:02:24 +0200

    Update index.tsx

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -44,7 +44,7 @@ const Header = ({ onMenuToggle }: HeaderProps): ReactElement => {
       <div className={classnames(css.element, css.hideMobile, css.logo)}>
         <Link href={logoHref} passHref>
           <a>
-            Decentra{Pro}
+            Decentra&#123;Pro&#125;
           </a>
         </Link>
       </div>
