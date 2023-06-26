9e5fe1928525f53e638ebcfaa8f44edc552c403c
nitegeist <nitegeist.dev@gmail.com>
2023-06-26 15:40:23 -0500

    Show 3 dots only when safe is active or on hover

--- src/components/folder-list/index.tsx
+++ src/components/folder-list/index.tsx
@@ -1,20 +1,28 @@
-import { AppRoutes } from '@/config/routes'
-import { useAllOwnedSafes } from '@/hooks/useAllOwnedSafes'
-import useSafeInfo from '@/hooks/useSafeInfo'
-import { ListItem, ListItemButton, Typography } from '@mui/material'
-import List from '@mui/material/List'
-import ListItemAvatar from '@mui/material/ListItemAvatar'
-import ListItemText from '@mui/material/ListItemText'
-import Link from 'next/link'
-import { memo, useEffect, useState } from 'react'
-import ellipsisAddress from '../../utils/ellipsisAddress'
-import Identicon from '../common/Identicon'
-import FolderListContextMenu from './folderItemContextItem'
+import { AppRoutes } from '@/config/routes';
+import { useAllOwnedSafes } from '@/hooks/useAllOwnedSafes';
+import useSafeInfo from '@/hooks/useSafeInfo';
+import { useAppSelector } from '@/store';
+import { selectSafe, setSelectedSafe } from '@/store/chatServiceSlice';
+import { ListItem, ListItemButton, Typography } from '@mui/material';
+import List from '@mui/material/List';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import { styled } from '@mui/material/styles';
+import Link from 'next/link';
+import { memo, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import ellipsisAddress from '../../utils/ellipsisAddress';
+import Identicon from '../common/Identicon';
+import FolderListContextMenu from './folderItemContextItem';
 
 const FolderList: React.FC = () => {
+  const dispatch = useDispatch()
+  const selectedSafe = useAppSelector((state) => selectSafe(state))
   const allOwnedSafes = useAllOwnedSafes()
   const [safeFolder, setSafeFolder] = useState<string[]>([])
   const { safeAddress } = useSafeInfo()
+  const [activeSafe, setActiveSafe] = useState<string>();
+
   //TODO: can be signficantly refactored
   useEffect(() => {
     if (allOwnedSafes?.size) {
@@ -33,16 +41,38 @@ const FolderList: React.FC = () => {
     }
   }, [allOwnedSafes])
 
+
+  const CustomListItem = styled(ListItem)(({ theme }) => ({
+    '&&.Mui-selected': {
+      backgroundColor: theme.palette.background.paper,
+      borderLeft: '5px solid #FE7E51'
+    },
+    '&&:hover': {
+      backgroundColor: theme.palette.border.light
+    },
+  }))
+
   const matchSafe = (safe: string) => {
     return safe.slice(safe.lastIndexOf(':') + 1) === safeAddress
   }
+  const handleMouseEnter = (safe: string) => {
+    setActiveSafe(safe)
+  };
+  const handleMouseLeave = () => {
+    setActiveSafe(undefined)
+  };
+  const handleClick = (safe: string) => {
+    dispatch(setSelectedSafe({ selectedSafe: safe }))
+  }
+
   return (
     <List sx={{ padding: '0px' }}>
       {safeFolder?.map((safe, index) => (
-        <ListItem key={`${safe}-${index}`} selected={matchSafe(safe)} sx={{ "&:hover": { backgroundColor: 'var(--color-border-light)' } }}>
+        <CustomListItem key={`${safe}-${index}`} selected={matchSafe(safe)} onMouseOver={(e) => handleMouseEnter(safe)} onMouseLeave={handleMouseLeave}>
           <Link href={{ pathname: AppRoutes.chat, query: { safe: `${safe}` } }} key={`${safe}-${index}`} passHref>
             <ListItemButton
               key={`safe-${index}`}
+              onClick={(e) => handleClick(safe)}
               sx={{
                 padding: '2px 6px', minHeight: '69px',
                 "&:hover": {
@@ -59,8 +89,8 @@ const FolderList: React.FC = () => {
               />
             </ListItemButton>
           </Link>
-          <FolderListContextMenu address={safe} />
-        </ListItem>
+          {(activeSafe === safe || selectedSafe === safe) && <FolderListContextMenu address={safe} />}
+        </CustomListItem>
       ))
       }
     </List >
