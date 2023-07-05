dcedf79d225d89d77b1b115bda755038f60e7585
nitegeist <nitegeist.dev@gmail.com>
2023-07-05 10:27:15 -0500

    Show safe list page on mobile, fix arrow not going to safe list

--- src/pages/safe-list.tsx
+++ src/pages/safe-list.tsx
@@ -1,40 +1,16 @@
 import MobileChatFooter from "@/components/chat/mobileChatFooter"
 import { SafeList } from "@/components/chat/SafeList"
-import useWallet from "@/hooks/wallets/useWallet"
-import { getSession, signOut } from "next-auth/react"
 import { useRouter } from "next/router"
 import React, { useEffect, useState } from "react"
 
-export async function getServerSideProps(context: any) {
-	const session = await getSession(context)
-	const path = context.req.url.split('?')
-	// redirect if not authenticated
-	if (!session) {
-		return {
-			redirect: {
-				destination: path[1] ? `/welcome?${path[1]}` : '/welcome',
-				permanent: false,
-			},
-		}
-	}
-
-	return {
-		props: { user: session.user },
-	}
-}
-
-const SafePage: React.FC<{ user: any }> = ({ user }) => {
+const SafePage: React.FC = () => {
 	const router = useRouter()
-	const wallet = useWallet()
 	const [createSafe, setCreateSafe] = useState<boolean>(false)
 	useEffect(() => {
-		if (user.address !== wallet?.address) {
-			signOut({ redirect: true })
-		}
 		if (router.asPath.includes('chain')) {
 			setCreateSafe(true)
 		}
-	}, [])
+	}, [router.asPath])
 	return (
 		<>
 			<SafeList createSafe={createSafe} setCreateSafe={setCreateSafe} />
