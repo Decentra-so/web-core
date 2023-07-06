a89cf8a63bac596bb052b9d86356077bb67c6129
Daniel <95552251+d3centra@users.noreply.github.com>
2023-07-06 19:32:59 +0200

    Update index.tsx

--- src/components/common/WalletInfo/index.tsx
+++ src/components/common/WalletInfo/index.tsx
@@ -3,8 +3,6 @@ import type { ReactElement } from 'react'
 
 import EthHashInfo from '@/components/common/EthHashInfo'
 import type { ConnectedWallet } from '@/hooks/wallets/useOnboard'
-import { useAppSelector } from '@/store'
-import { selectChainById } from '@/store/chainsSlice'
 
 import css from './styles.module.css'
 
