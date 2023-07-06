74f6836e20128e380910a530e84e00e39eb0d4d8
Daniel <95552251+d3centra@users.noreply.github.com>
2023-07-06 19:18:58 +0200

    Update ConnectionCenter.tsx

--- src/components/common/ConnectWallet/ConnectionCenter.tsx
+++ src/components/common/ConnectWallet/ConnectionCenter.tsx
@@ -3,7 +3,6 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import ExpandLessIcon from '@mui/icons-material/ExpandLess'
 import { useState, type MouseEvent, type ReactElement } from 'react'
 
-import KeyholeIcon from '@/components/common/icons/KeyholeIcon'
 import WalletDetails from '@/components/common/ConnectWallet/WalletDetails'
 import PairingDetails from '@/components/common/PairingDetails'
 
