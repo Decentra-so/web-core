14cb211db41c474d8cef92e7d03b893c8cc0b36e
Daniel <95552251+d3centra@users.noreply.github.com>
2023-07-09 16:20:15 +0200

    Create AddNewTxLightningIconButton.tsx

---
+++ src/components/chat/AddNewTxLightningIconButton.tsx
@@ -0,0 +1,38 @@
+import CheckWallet from '@/components/common/CheckWallet'
+import { OVERVIEW_EVENTS, trackEvent } from '@/services/analytics'
+import AddIcon from '@/public/images/chat/lightning-svgrepo-com.svg'
+import { IconButton } from '@mui/material'
+import dynamic from 'next/dynamic'
+import { Suspense, useState, type ReactElement } from 'react'
+
+const NewTxModal = dynamic(() => import('@/components/tx/modals/NewTxModal'))
+
+const AddNewTxLightningIconButton = (): ReactElement => {
+  const [txOpen, setTxOpen] = useState<boolean>(false)
+
+  const onClick = () => {
+    setTxOpen(true)
+
+    trackEvent(OVERVIEW_EVENTS.NEW_TRANSACTION)
+  }
+
+  return (
+    <>
+      <CheckWallet allowSpendingLimit>
+        {(isOk) => (
+          <IconButton aria-label="add transaction" onClick={onClick} disabled={!isOk} margin="0 6px">
+            <AddIcon />
+          </IconButton>
+        )}
+      </CheckWallet>
+
+      {txOpen && (
+        <Suspense>
+          <NewTxModal onClose={() => setTxOpen(false)} />
+        </Suspense>
+      )}
+    </>
+  )
+}
+
+export default AddNewTxLightningIconButton
