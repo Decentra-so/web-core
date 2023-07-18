7c8242a61fef5b1ab6c424569ef63ae309c86868
Daniel <95552251+d3centra@users.noreply.github.com>
2023-07-18 18:06:32 +0200

    Update chat.ts

--- src/services/chat.ts
+++ src/services/chat.ts
@@ -106,7 +106,7 @@ const joinGroup = async (GUID: any) => {
 }
 
 const getMessages = async (UID: any) => {
-  const limit = 1000
+  const limit = 200
   const messagesRequest = new CometChat.MessagesRequestBuilder().setGUID(UID).setLimit(limit).build()
 
   return new Promise(async (resolve, reject) => {
