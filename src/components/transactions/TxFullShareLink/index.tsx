b6d7b1b8fd1a960a675185c8eeae88023cb8e476
Daniel <95552251+d3centra@users.noreply.github.com>
2023-07-19 23:33:54 +0200

    Create index.tsx

---
+++ src/components/transactions/TxFullShareLink/index.tsx
@@ -0,0 +1,25 @@
+import type { ReactElement } from 'react'
+import { Link } from '@mui/material'
+import { AppRoutes } from '@/config/routes'
+import { useRouter } from 'next/router'
+import Track from '@/components/common/Track'
+import { TX_LIST_EVENTS } from '@/services/analytics/events/txList'
+import React, { useState } from 'react'
+
+const TxFullShareLink = ({ id }: { id: string }): ReactElement => {
+  const [isCopyEnabled, setIsCopyEnabled] = useState(true)
+
+  const router = useRouter()
+  const { safe = '' } = router.query
+  const href = `${AppRoutes.transactions.tx}?safe=${safe}&id=${id}`
+
+  return (
+    <Track {...TX_LIST_EVENTS.COPY_DEEPLINK}>
+      <Link href={href}>
+        View more details
+      </Link>
+    </Track>
+  )
+}
+
+export default TxFullShareLink
