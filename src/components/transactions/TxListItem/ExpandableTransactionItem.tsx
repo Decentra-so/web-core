dafe52e1fc97abcc5ae51197c799f543df1d5add
Daniel <95552251+d3centra@users.noreply.github.com>
2023-07-19 22:07:18 +0200

    Update ExpandableTransactionItem.tsx

--- src/components/transactions/TxListItem/ExpandableTransactionItem.tsx
+++ src/components/transactions/TxListItem/ExpandableTransactionItem.tsx
@@ -60,7 +60,7 @@ export const TransactionSkeleton = () => (
     </Box>
 
     <Accordion disableGutters elevation={0} defaultExpanded className={css.accordion}>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{ justifyContent: 'flex-start', overflowX: 'auto' }}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{ justifyContent: 'flex-start', overflowX: 'auto', padding: '0' }}>
         <Skeleton width="100%" />
       </AccordionSummary>
 
