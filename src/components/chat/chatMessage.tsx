66135cb10fc38c55427ad7b9903ff47fc34a435c
Daniel <95552251+d3centra@users.noreply.github.com>
2023-07-20 18:51:26 +0200

    Update chatMessage.tsx

--- src/components/chat/chatMessage.tsx
+++ src/components/chat/chatMessage.tsx
@@ -29,7 +29,7 @@ const ChatMessage: React.FC<{ chat: any, wallet: ConnectedWallet | null }> = ({ 
 			sx={{
 			background: 'var(--color-background-papercolor)',
                         padding: '8px 12px',
-                        borderRadius: '0px 16px 16px 16px',
+                        borderRadius: '0 16px 16px 16px',
 		}}
 			primary={
 				<Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
