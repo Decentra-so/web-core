52103da3200b68ba038e98eef3c1f6edcf397e41
Daniel <95552251+d3centra@users.noreply.github.com>
2023-07-23 00:10:26 +0200

    Update index.tsx

--- src/components/chat/modals/CreateSafe/index.tsx
+++ src/components/chat/modals/CreateSafe/index.tsx
@@ -1,65 +1,16 @@
 import ModalDialog from '@/components/common/ModalDialog'
 import Open from '@/pages/new-safe/create'
-import Load from '@/pages/new-safe/load'
-import React, { useState } from 'react'
-import {
-  DialogContent,
-  Tab,
-  Tabs,
-  Box,
-  Typography
-} from '@mui/material'
-
-interface TabPanelProps {
-  children?: React.ReactNode
-  index: number
-  value: number
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 1.5 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  )
-}
-
+import { DialogContent } from '@mui/material'
+import React from 'react'
 
 const ViewCreateSafe: React.FC<{
   open: boolean
   onClose: () => void
 }> = ({ open, onClose }) => {
-  const [tab, setTab] = useState<number>(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setTab(newValue)
-  }
-
   return (
     <ModalDialog open={open} dialogTitle="Add Safe" onClose={onClose} maxWidth="md">
       <DialogContent sx={{ maxHeight: '90vh', overflow: 'auto' }}>
-        <Tabs value={tab} onChange={handleChange} aria-label="folder tabs">
-          <Tab label="Create" />
-          <Tab label="Load" />
-        </Tabs>
-        <TabPanel value={tab} index={0}>
-          <Open />
-        </TabPanel>
-        <TabPanel value={tab} index={1}>
-          <Load />
-        </TabPanel>
+        <Open />
       </DialogContent>
     </ModalDialog>
   )
