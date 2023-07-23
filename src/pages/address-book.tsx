2e2427fd94dad32f0dac41102407d2522937d0aa
Daniel <95552251+d3centra@users.noreply.github.com>
2023-07-23 19:32:17 +0200

    Update address-book.tsx

--- src/pages/address-book.tsx
+++ src/pages/address-book.tsx
@@ -1,4 +1,3 @@
-import NetworkSelector from '@/components/common/NetworkSelector'
 import AddressBookTable from '@/components/address-book/AddressBookTable'
 import MobileChatFooter from '@/components/chat/mobileChatFooter'
 import type { NextPage } from 'next'
@@ -10,10 +9,6 @@ const AddressBook: NextPage = () => {
       <Head>
         <title>{'Decentra{Pro} – Address book'}</title>
       </Head>
-      
-      <div>
-        <NetworkSelector />
-      </div>
       
       <AddressBookTable />
       <MobileChatFooter />
