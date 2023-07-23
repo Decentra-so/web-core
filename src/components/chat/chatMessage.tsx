13fe7992542036cb1097d90bf9743c669957532b
Daniel <95552251+d3centra@users.noreply.github.com>
2023-07-23 15:17:14 +0200

    Update chatMessage.tsx

--- src/components/chat/chatMessage.tsx
+++ src/components/chat/chatMessage.tsx
@@ -40,8 +40,7 @@ const ChatMessage: React.FC<{ chat: any, wallet: ConnectedWallet | null }> = ({ 
 			secondary={
 				<Typography sx={{ display: 'inline', pt: '10px' }} component="span">
 					{chat.data.text}
-					<br/>
-					<Typography sx={{ display: 'inline', color: 'gray', ml: '10px', fontSize: '12px' }} component="span">
+					<Typography sx={{ display: 'inline', color: 'gray', ml: '20px', fontSize: '12px' }} component="span">
 						{getDateFromTimestamp(chat.data.sentAt)}
 					</Typography>
 				</Typography>
