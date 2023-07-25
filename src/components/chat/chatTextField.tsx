38d327729737fbf73a36e68fb9276fae15bd35ec
sky cherfa <sherifcherfa@gmail.com>
2023-07-25 06:32:22 -0400

    clean up

--- src/components/chat/chatTextField.tsx
+++ src/components/chat/chatTextField.tsx
@@ -1,9 +1,9 @@
 import useSafeAddress from "@/hooks/useSafeAddress";
-import { getMessages, listenForMessage, sendMessage } from "@/services/chat";
+import { sendMessage } from "@/services/chat";
 import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 import { Button, Divider, InputBase, Paper, Box, Typography } from "@mui/material";
 import { styled } from '@mui/material/styles';
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import AddNewTxLightningIconButton from "./AddNewTxLightningIconButton";
 import SignInLink from "../auth-sign-in/auth-link";
 
@@ -24,26 +24,6 @@ const ChatTextField: React.FC<{
 }> = ({ currentUser, messages, setMessages, authToken, setAuth }) => {
 	const safeAddress = useSafeAddress()
 	const [message, setMessage] = useState<string>('')
-
-	//get and listen for message updates
-	useEffect(() => {
-		async function getM() {
-			await getMessages(`pid_${safeAddress!}`)
-				.then((msgs: any) => {
-					setMessages(msgs)
-				})
-				.catch((error) => {
-					setMessages([])
-				})
-
-			await listenForMessage(`pid_${safeAddress!}`)
-				.then((msg: any) => {
-					setMessages((prevState: any) => [...prevState, msg])
-				})
-				.catch((error) => console.log(error))
-		}
-		getM()
-	}, [safeAddress, currentUser])
 
 	const handleSubmit = async (e: any) => {
 		e.preventDefault()
