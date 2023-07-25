57619fcf5a029e3fcc72c1311c43676cbcd9da5c
sky cherfa <sherifcherfa@gmail.com>
2023-07-25 07:57:28 -0400

    gm

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = (api) => {
+  api.cache(true)
+  const plugins = [
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-syntax-bigint'
+  ]
+  return { plugins }
+}
