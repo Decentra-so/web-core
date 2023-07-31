2da456604a788853521ee681697a08e672d82a7d
Daniel <95552251+d3centra@users.noreply.github.com>
2023-07-31 21:49:18 +0200

    Update threshold.tsx

--- src/components/chat/threshold.tsx
+++ src/components/chat/threshold.tsx
@@ -4,10 +4,7 @@ import { Box, Typography } from '@mui/material'
 export const ThresholdOverview = ({ threshold, owners }: { threshold: number; owners: number }) => {
   return (
     <Box sx={{ display: 'flex', gap: '6px' }}>
-          <Typography>
-            {threshold} out of {owners} members
-          </Typography>
-
+            {threshold}/{owners} THRESHOLD
           {owners > 1 && <ChangeThresholdDialogEditIcon />}
     </Box>
   )
