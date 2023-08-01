cd35d00dfbccd097ae9932ce20c0e4e872f0ded2
Daniel <95552251+d3centra@users.noreply.github.com>
2023-08-01 14:28:13 +0200

    Update SafeTagsInfo.tsx

--- src/components/chat/SafeTagsInfo.tsx
+++ src/components/chat/SafeTagsInfo.tsx
@@ -6,7 +6,6 @@ import useSafeInfo from '@/hooks/useSafeInfo'
 import { useAppSelector } from '@/store'
 import { selectCurrency } from '@/store/settingsSlice'
 
-import { useCurrentChain } from '@/hooks/useChains'
 import { useVisibleBalances } from '@/hooks/useVisibleBalances'
 
 const SafeTagsInfo = (): ReactElement => {
@@ -14,7 +13,6 @@ const SafeTagsInfo = (): ReactElement => {
   const { balances } = useVisibleBalances()
   const { safe } = useSafeInfo()
   const { threshold, owners } = safe
-  const chain = useCurrentChain()
 
   const fiatTotal = useMemo(
     () => (balances.fiatTotal ? formatCurrency(balances.fiatTotal, currency) : ''),
@@ -24,7 +22,19 @@ const SafeTagsInfo = (): ReactElement => {
   return (
      <Box sx={{ display: 'flex', gap: '4px' }}>
           <Box sx={{ width: 'auto', height: '20px', borderRadius: '4px', fontSize: '12px', padding: '6px', alignItems: 'center', display: 'flex', textTransform: 'uppercase', fontWeight: '600', color: '#517ac6', background: '#dce5f5' }}>
-            {chain}
+            {safe?.chainId === '137'
+              ? 'Polygon'
+              : safe?.chainId === '1'
+                ? 'Ethereum'
+                : safe?.chainId === '10'
+                  ? 'Optimism'
+                  : safe?.chainId === '42161'
+                    ? 'Arbitrum'
+                    : safe?.chainId === '56'
+                      ? 'BNB Smart Chain'
+                      : safe?.chainId === '100'
+                        ? 'Gnosis Chain'
+                        : ''}
           </Box>
           <Box sx={{ width: 'auto', height: '20px', borderRadius: '4px', fontSize: '12px', padding: '6px', alignItems: 'center', display: 'flex', textTransform: 'uppercase', fontWeight: '600', color: '#7b5aa9', background: '#e4e0ed' }}>
             {fiatTotal}
