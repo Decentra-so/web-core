10a5acf4732e8205dde9e9867453426cefce7ed5
Sero <69639595+Seroxdesign@users.noreply.github.com>
2023-08-10 07:56:23 -0400

    Update index.ts

--- src/services/monerium/index.ts
+++ src/services/monerium/index.ts
@@ -1,6 +1,6 @@
 import { MoneriumPack } from '@safe-global/onramp-kit'
 
 export const moneriumPack = new MoneriumPack({
-  clientId: '3af9423d-36a6-11ee-aa67-8ac0dd3eab06', // Get your client id from Monerium
+  clientId: 'e7d57bec-3773-11ee-94a6-369e770da2e3', // Get your client id from Monerium
   environment: 'sandbox' // Use the proper Monerium environment ('sandbox' | 'production')})
 })
